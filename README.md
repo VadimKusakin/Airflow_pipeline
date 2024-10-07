f7d586e31988c8fd42537850b63375f0b38ba811
VadimKusakin <127764120+VadimKusakin@users.noreply.github.com>
2024-10-07 15:06:39 +0500

    Commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Sales Data Pipeline
+
+This project implements an automated pipeline for generating, processing, analyzing, and migrating sales data using a combination of **PostgreSQL**, **ClickHouse**, **Apache Airflow**, and **PySpark**. The pipeline is designed to handle 1 million sales records, clean the data, perform aggregations, and store the results in both PostgreSQL and ClickHouse for further analysis.
+
+
+### Key Technologies
+
+- **PostgreSQL**: Used for storing raw and processed sales data.
+- **ClickHouse**: Used for storing aggregated data for fast analytical queries.
+- **Apache Airflow**: Orchestrates the pipeline, schedules and automates tasks.
+- **PySpark**: Used for large-scale data processing, cleaning, and analysis.
+- **Docker**: Manages the multi-service architecture, including adding PySpark to the Airflow container.
+
+### Pipeline Workflow
+
+1. **DAG Execution**: The Airflow DAG starts at 12:45 Moscow time every Tuesday.
+2. **Data Generation**: The pipeline generates 1 million realistic sales records with the following fields:
+   - `sale_id` (Unique sale identifier)
+   - `customer_id` (Customer identifier)
+   - `product_id` (Product identifier)
+   - `quantity` (Number of products sold)
+   - `sale_date` (Date of the sale)
+   - `price` (Random product price)
+   - `region` (Customer's region: North, South, East, or West)
+3. **Data Cleaning**: The pipeline ensures all data fields are in the correct format for further processing.
+4. **PostgreSQL Insertion**: Cleaned data is loaded into a PostgreSQL table for raw sales data.
+5. **Data Aggregation**: Using window functions and groupings, the following metrics are calculated:
+   - Total sales and sales amounts per region and product.
+   - Average sale amount (`average_sale_amount`) per region and product.
+6. **Data Migration to ClickHouse**: Aggregated data is migrated from PostgreSQL to ClickHouse, with an additional `import_date` field to track the import time.
+
+### Viewing Data via DBeaver
+
+After the data has been successfully loaded into PostgreSQL and ClickHouse, you can use **DBeaver** to connect and view the data.
+
+#### Connecting to PostgreSQL:
+
+   - **Host**: `localhost`
+   - **Port**: `5433`
+   - **Database**: `test`
+   - **Username**: `user`
+   - **Password**: `password`
+
+#### Connecting to ClickHouse:
+
+   - **Host**: `localhost`
+   - **Port**: `8123`
+
+
+
+## Setup Instructions
+
+This project was developed on a MacBook with an M-series processor (ARM architecture). It has not been tested on other architectures, so there is no guarantee that the Docker image and containers will run correctly on different systems.
+
+   ```bash
+   git clone https://github.com/VadimKusakin/Airflow_pipeline.git
+   cd Airflow_pipeline
+   docker build -t airflow-with-java .
+   docker-compose up
+   ```
+
+# Contact
+https://t.me/KusakinVadim
+
+For any questions or issues regarding the project, feel free to contact the repository owner.
---
+++ dags/main.py
@@ -0,0 +1,172 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+import csv
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql.types import IntegerType, StringType, FloatType, DateType
+import random
+from datetime import datetime, timedelta
+import psycopg2
+from clickhouse_connect.driver import create_client
+import pendulum
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+}
+
+dag = DAG(
+    'main',
+    description='A simple DAG to interact with ClickHouse and PostgreSQL',
+    default_args=default_args,
+    catchup=False,
+    start_date=datetime(2024, 1, 1, tzinfo=pendulum.timezone("Europe/Moscow")),
+    schedule_interval='45 12 * * 2',
+)
+
+
+file_path = "data.csv"
+
+def generate_sales_data():
+    def random_date():
+        return datetime.now() - timedelta(days=random.randint(0, 365))
+
+    num_records = 1000000
+
+    with open(file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['sale_id', 'customer_id', 'product_id', 'quantity', 'sale_date', 'price', 'region'])
+        
+        for i in range(num_records):
+            sale_id = i+1
+            customer_id = random.randint(1, 10000)
+            product_id = random.randint(1, 100)
+            quantity = random.randint(1, 20)
+            sale_date = random_date()
+            price = random.randint(10, 1000)
+            region = random.choice(["North", "South", "East", "West"])
+            
+            writer.writerow([sale_id, customer_id, product_id, quantity, sale_date, price, region])
+
+    print(f"{num_records} records were generated and saved in the {file_path}")
+
+def transform_sales_data_and_load_to_PG():
+    spark = SparkSession.builder \
+        .appName("SalesDataGeneration") \
+        .config("spark.jars", "/opt/spark/jars/postgresql-42.7.4.jar") \
+        .master("local[*]") \
+        .getOrCreate()
+    sales_df = spark.read.csv(file_path, header=True, inferSchema=True)
+    sales_df = sales_df.withColumn("sale_amount", col("quantity") * col("price"))
+
+    sales_df = sales_df.withColumn("sale_id", col("sale_id").cast(IntegerType())) \
+                       .withColumn("customer_id", col("customer_id").cast(IntegerType())) \
+                       .withColumn("product_id", col("product_id").cast(IntegerType())) \
+                       .withColumn("quantity", col("quantity").cast(IntegerType())) \
+                       .withColumn("sale_amount", col("sale_amount").cast(FloatType())) \
+                       .withColumn("sale_date", col("sale_date").cast(DateType())) \
+                       .withColumn("price", col("price").cast(IntegerType())) \
+                       .withColumn("region", col("region").cast(StringType()))
+
+    sales_df.show(12)
+
+    sales_df.write \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://host.docker.internal:5433/test") \
+        .option("dbtable", "sales_data") \
+        .option("user", "user") \
+        .option("password", "password") \
+        .option("driver", "org.postgresql.Driver") \
+        .mode("overwrite") \
+        .save()
+
+    spark.stop()
+
+pg_conn = psycopg2.connect(
+        host="host.docker.internal",
+        port="5433",
+        database="test",
+        user="user",
+        password="password"
+    )
+cursor = pg_conn.cursor()
+
+def aggregate_data():
+    cursor.execute("""
+                CREATE TABLE IF NOT EXISTS aggregated_sales_data (
+                    product_id int,
+                    region varchar,
+                    num_of_sales int,
+                    total_sum int,
+                    average_sale_amount numeric(76, 2));
+                
+                TRUNCATE aggregated_sales_data;
+
+                INSERT INTO aggregated_sales_data (product_id, region, num_of_sales, total_sum, average_sale_amount)
+                SELECT product_id,
+                    region,
+                    COUNT(*),
+                    SUM(sale_amount),
+                    SUM(sale_amount)/COUNT(*)
+                FROM sales_data
+                GROUP BY (region, product_id)
+                ORDER BY product_id, region;
+            """)
+    pg_conn.commit()
+
+clickhouse_client = create_client(
+        host='host.docker.internal',
+        port=8123,
+        username='',
+        password=''
+    )
+
+def load_data_to_CH():
+    cursor.execute("SELECT * FROM aggregated_sales_data")
+    rows = cursor.fetchall()
+
+    clickhouse_client.command("""
+                    CREATE TABLE IF NOT EXISTS aggregated_sales_data (
+                        product_id Int32,
+                        region String,
+                        num_of_sales Int32,
+                        total_sum Int32,
+                        average_sale_amount Decimal(76, 2),
+                        import_time DateTime DEFAULT now()
+                    ) ENGINE = MergeTree()
+                    ORDER BY (import_time, product_id);
+                    """)
+
+    clickhouse_client.insert('aggregated_sales_data', rows,
+                                column_names=['product_id', 'region', 'num_of_sales', 'total_sum', 'average_sale_amount'])
+
+    cursor.close()
+    pg_conn.close()
+
+
+task_generate_data = PythonOperator(
+    task_id='generate_sales_data',
+    python_callable=generate_sales_data,
+    dag=dag,
+)
+
+task_transform_and_load = PythonOperator(
+    task_id='transform_data_and_load_to_PG',
+    python_callable=transform_sales_data_and_load_to_PG,
+    dag=dag,
+)
+
+task_aggregate_data = PythonOperator(
+    task_id='aggregate_data',
+    python_callable=aggregate_data,
+    dag=dag,
+)
+
+task_load_to_CH = PythonOperator(
+    task_id='load_data_to_CH',
+    python_callable=load_data_to_CH,
+    dag=dag,
+)
+
+task_generate_data >> task_transform_and_load >> task_aggregate_data >> task_load_to_CH
